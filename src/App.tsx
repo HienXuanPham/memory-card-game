058950690c3972ec5f88620398fac67190077dcf
HienXuanPham <44250274+HienXuanPham@users.noreply.github.com>
2024-01-16 19:16:27 -0500

    add logic for the game (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [dogs, setDogs] = useState([]);
-  const [reset, setReset] = useState(false);
+  const [dogs, setDogs] = useState<string[]>([]);
+  const [reset, setReset] = useState<boolean>(false);
+  const [dogSelected, setDogSelected] = useState<string[]>([]);
+  const [score, setScore] = useState<number>(0);
 
   useEffect(() => {
     if (reset) {
@@ -23,6 +25,42 @@ function App() {
     };
   }, [reset]);
 
+  const handleDogSelected = (dog: string) => {
+    if (dogSelected.includes(dog)) {
+      window.alert("Dog is already selected. You lose!");
+      setDogSelected([]);
+      setScore(0);
+    } else if (score === dogs.length - 1) {
+      window.alert("You won!");
+      setDogSelected([]);
+      setScore(0);
+    } else {
+      setDogSelected((prevDogs) => [...prevDogs, dog]);
+      setScore((score) => score + 1);
+      setDogs((prevDogs) => shuffleDogsCard(prevDogs));
+    }
+  };
+
+  const shuffleDogsCard = (a: string[]) => {
+    let shuffleDogs: string[] = [];
+
+    shuffleDogs = a.slice();
+    let currentIndex = a.length;
+    let randomIndex: number;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [shuffleDogs[currentIndex], shuffleDogs[randomIndex]] = [
+        shuffleDogs[randomIndex],
+        shuffleDogs[currentIndex],
+      ];
+    }
+
+    return shuffleDogs;
+  };
+
   return (
     <>
       <h1>Memory Dog Card Game</h1>
@@ -33,10 +71,15 @@ function App() {
       >
         Start Game
       </button>
+      <h3>Score: {score}</h3>
       <ul>
         {dogs.map((dog, index) => (
           <li key={index}>
-            <img src={dog} alt={`A cute dog ${index + 1}`} />
+            <img
+              src={dog}
+              alt={`A cute dog ${index + 1}`}
+              onClick={() => handleDogSelected(dog)}
+            />
           </li>
         ))}
       </ul>
